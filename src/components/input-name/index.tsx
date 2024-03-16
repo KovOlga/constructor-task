4dcaed2db1a8de30f7b0ecdb66702bab0629f2ba
KovOlga <switch.to.olga@yandex.ru>
2024-03-16 19:21:42 +0300

    added validation on input name

--- src/components/input-name/index.tsx
+++ src/components/input-name/index.tsx
@@ -1,26 +1,28 @@
 import { ChangeEvent, FC, useState } from "react";
 import { getAgeByName } from "../../services/api";
 import { Button, Div, FormItem, Group, Input } from "@vkontakte/vkui";
-import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
+
+const regex = /\W+|\d/;
 
 const InputName: FC = () => {
   const [name, setName] = useState("olga");
   const [age, setAge] = useState(0);
   const [enabled, setEnabled] = useState(false);
+  const [isInputError, setIsInputError] = useState(false);
   const queryClient = useQueryClient();
-  let timerId;
+  let timerId: number;
 
-  const fetchVGV = async (name: string) => {
-    console.log("res");
-    const data = await getAgeByName(name);
-    console.log("data", data);
-    return data;
-  };
-  const { data, isLoading, isError } = useQuery({
+  const { isLoading } = useQuery({
     queryKey: ["age"],
-    queryFn: () => fetchVGV(name),
+    queryFn: async () => {
+      const data = await getAgeByName(name);
+      setAge(data.age);
+      return data;
+    },
     refetchOnWindowFocus: false,
     enabled: enabled,
+    retry: false,
   });
 
   const getAgeBtnClick = () => {
@@ -29,10 +31,14 @@ const InputName: FC = () => {
     if (isLoading) {
       queryClient.cancelQueries({ queryKey: ["age"] });
     }
-    getAgeByName(name).then((res) => {
-      console.log("res", res);
-      setAge(res.age);
-    });
+    getAgeByName(name)
+      .then((res) => {
+        console.log("res", res);
+        setAge(res.age);
+      })
+      .catch((err: { error: string }) => {
+        console.log("error", err);
+      });
   };
 
   const countDownOnInputBlur = () => {
@@ -42,20 +48,30 @@ const InputName: FC = () => {
     }, 3000);
   };
 
-  /////////////////
-  const onInputChangesws = (e: ChangeEvent<HTMLInputElement>) => {
+  const onInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const found = e.target.value.match(regex);
+    if (found) {
+      setIsInputError(true);
+      return;
+    } else {
+      setIsInputError(false);
+    }
     setName(e.target.value);
   };
 
   return (
     <Group>
       <Button onClick={getAgeBtnClick}>Узнать возраст</Button>
-      <FormItem htmlFor="exampleec">
+      <FormItem
+        htmlFor="name"
+        bottom={!isInputError ? "" : "Должны быть только буквы"}
+        status={!isInputError ? "valid" : "error"}
+      >
         <Input
           type="text"
-          id="exampleec"
+          id="name"
           value={name}
-          onChange={onInputChangesws}
+          onChange={onInputChange}
           onBlur={countDownOnInputBlur}
         />
       </FormItem>
